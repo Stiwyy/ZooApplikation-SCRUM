dbe5b98a8be7d9a3840e62642d6ff563f9a6873e
Lorenz Held <held.lorenz.08@gmail.com>
2025-03-12 08:54:42 +0100

    Map With 100%

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import './App.css'
 import Map from "./assets/Map.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Map/>
+        <Map/>
     </>
   )
 }
